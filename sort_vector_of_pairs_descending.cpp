c57bec976f5ba455d7ef795f3436bc8957b55531
Rishabh Bafna <rishabh20118@iiitd.ac.in>
2025-10-25 01:13:55 +0530

    Implement sorting of vector of pairs in descending order
    This program sorts a vector of pairs in descending order based on the first element of each pair.

---
+++ sort_vector_of_pairs_descending.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    vector<pair<int,int>> v;
+    int n;
+    cin>>n;
+    for(int i = 0 ; i < n ; i++)
+    {
+        int a,b;
+        cin>>a>>b;
+        v.push_back({a,b});
+    }
+
+    sort(v.begin(), v.end(), greater<pair<int,int>>());
+
+    for(pair<int,int> p : v)
+        cout<<p.first<<" "<<p.second<<endl;
+    return 0;
+}
