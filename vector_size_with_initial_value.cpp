c3ee3db167bb178ee5360b5f31f00c75e3b3ca91
Rishabh Bafna <rishabh20118@iiitd.ac.in>
2025-10-25 01:01:12 +0530

    Add vector size and capacity example

---
+++ vector_size_with_initial_value.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v(5);
+    cout<<"Vector size = "<<v.size()<<endl;
+    cout<<"Vector capacity = "<<v.capacity()<<endl;
+    return 0;
+}
